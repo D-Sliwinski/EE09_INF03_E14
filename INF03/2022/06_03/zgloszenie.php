5a4354f932c561c8fea9a3176c191d91c63ba7a1
D-Sliwinski <dominik.j.sliwinski@gmail.com>
2022-12-22 19:46:44 +0100

    Update

--- INF03/2022/06_03/zgloszenie.php
+++ INF03/2022/06_03/zgloszenie.php
@@ -1,7 +1,7 @@
 <?php
     if(isset($_POST["submit"])){
         $conn = mysqli_connect("localhost","root","","2022_06_03");
-        $sql = "INSERT INTO zawody_wedkarskie (`Karty_wedkarskie_id`,`Lowisko_id`,`data_zawodow`,`sedzia`) VALUES (0,$_POST[lowisko],'$_POST[data]','$_POST[sedzia]')";
+        $sql = "INSERT INTO zawody_wedkarskie (`Karty_wedkarskie_id`,`Lowisko_id`,`data_zawodow`,`sedzia`) VALUES (0,$_POST[lowisko],$_POST[data],'$_POST[sedzia]')";
         $query = mysqli_query($conn, $sql);
         mysqli_close($conn);
     }
